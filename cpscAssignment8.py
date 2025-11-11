6a1f33b73438138cdd9767701092c779d207d7e2
Jsuho02 <joshua.suhonen@gmail.com>
2025-11-11 13:07:19 -0500

    still testing the commit

--- cpscAssignment8.py
+++ cpscAssignment8.py
@@ -1,3 +1,12 @@
 # main()- first function requesting decimal or hexadecimal input
+def program_start():
+    
+    while True:
+        choose = input("Will you be entering a decimal or hexa decimal value? (d/h): ")
 
-
+        if choose == 'd':
+            num_str = input("Please enter your decimal number: ")
+            num = int(num_str)
+            print(f"Your number is {num}.")
+        
+    
