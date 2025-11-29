f469c1aa8d6d14143ff33299d4816e2e7ba163dc
Jsuho02 <joshua.suhonen@gmail.com>
2025-11-29 17:10:17 -0500

    first function contiuation

--- cpscAssignment8.py
+++ cpscAssignment8.py
@@ -2,11 +2,15 @@
 def program_start():
     
     while True:
-        choose = input("Will you be entering a decimal or hexa decimal value? (d/h): ")
+        choice = input("Will you be entering a decimal or hexa decimal value? (d/h): ")
 
-        if choose == 'd':
+        if choice == 'd':
             num_str = input("Please enter your decimal number: ")
-            num = int(num_str)
-            print(f"Your number is {num}.")
-        
-    
+            try:
+                num = int(num_str)
+                print(f"Your number is {num}.")
+                return choice, num_str
+            except ValueError:
+                print("That is not a valid decimal number!")
+
+
