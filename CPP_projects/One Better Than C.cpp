b732ca4ed0c4b6c865743d2501f604342e8c8db6
Jsuho02 <joshua.suhonen@gmail.com>
2025-12-02 20:51:05 -0500

    fixed errors in the function to get the input

--- CPP_projects/One Better Than C.cpp
+++ CPP_projects/One Better Than C.cpp
@@ -2,11 +2,12 @@
 #include<string>
 #include<bitset>
 using namespace std;
+
 //function to check hex or decimal
 string parse_input(string input)
 {
-    string input(){
-        cout<<"Enter a number that is either decimal or hexadecimal">>;
-        cin>>input:
+    string input();
+        cout<<"Enter a number that is either decimal or hexadecimal: ";
+        cin>>input;
+        return input;
     }
-}
