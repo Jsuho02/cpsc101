3f23abc4835f18f6a58f52c43c283d054c3fc8bb
Jsuho02 <joshua.suhonen@gmail.com>
2025-12-02 21:06:10 -0500

    fixing the string parse_input and the string input

--- CPP_projects/OneBetterThanC.cpp
+++ CPP_projects/OneBetterThanC.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 //function to check hex or decimal
-string parse_input(string input)
+string parse_input()
 {
-    string input();
+    string input;
         cout<<"Enter a number that is either decimal or hexadecimal: ";
         cin>>input;
         return input;
